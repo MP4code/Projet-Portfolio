55fa4f91fafbd7dbebe6b43ba8e353810c1959d8
Marina <marina.philogene@gmail.com>
2024-10-10 15:38:54 +0200

    ajout navbar et debut page home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+// src/App.js
+import React from 'react';
+import Navbar from './Navbar';
+import './style/index.css';
+import monImage from './style/img/home.png'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <section id="home" className="section">
+                <div className='backgroundDecor'></div>
+                <img className="homeImage" src={monImage} alt='tasse de café' />
+                <h2 className='homeTitle'>PORTFOLIO</h2>
+
+                <div className='helloAutocollant'>
+                    Hello
+                </div>
+            </section>
+            <section id="about" className="section">
+                <h2>A propos</h2>
+            </section>
+            <section id="education" className="section">
+                <h2>Education</h2>
+            </section>
+            <section id="projets" className="section">
+                <h2>Mes Projets</h2>
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,53 @@
+// src/Navbar.js
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';
+import "./style/navbar.css"
+
+const Navbar = () => {
+    const [scroll, setScroll] = useState(false);
+
+    // Gérer la modification de la classe au scroll
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScroll(true);
+            } else {
+                setScroll(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <nav className={`navbar ${scroll ? 'scroll' : ''}`}>
+            <ul>
+                <li>
+                    <Link to="home" spy={true} smooth={true} offset={-70} duration={500}>
+                        Accueil
+                    </Link>
+                </li>
+                <li>
+                    <Link to="about" spy={true} smooth={true} offset={-70} duration={500}>
+                        A Propos
+                    </Link>
+                </li>
+                <li>
+                    <Link to="education" spy={true} smooth={true} offset={-70} duration={500}>
+                        Education
+                    </Link>
+                </li>
+                <li>
+                    <Link to="projets" spy={true} smooth={true} offset={-70} duration={500}>
+                        Mes Projets
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
